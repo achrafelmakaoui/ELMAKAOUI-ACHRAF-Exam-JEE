aef926a0219d8d64ba58e05e5f7c37b5f5d3dec8
Achraf EL MAKAOUI <elmakaouiachraf@gmail.com>
2026-05-11 14:45:21 +0100

    Spring Project Structure - Initialisation

---
+++ LocationVehicules-Backend/src/main/java/com/elmakaoui/locationvehiculesbackend/LocationVehiculesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elmakaoui.locationvehiculesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationVehiculesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationVehiculesBackendApplication.class, args);
+    }
+
+}
---
+++ LocationVehicules-Backend/src/test/java/com/elmakaoui/locationvehiculesbackend/LocationVehiculesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmakaoui.locationvehiculesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVehiculesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
